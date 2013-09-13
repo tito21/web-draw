702f2139da8d900e613335e62e216013090bee42
alberto <alberto.di.b.o@gmail.com>
2013-09-13 00:08:08 -0300

    load any img from the web

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ web-draw
 This project wil (hopfully) a simple web image editor to help non-tech people to how like to paint and draw by reference to take the maximun from their images.
 
 What I will like to do:
-<li><ul>load a file from the web</ul>
-<ul>draw a secure grid over the image and calculate the bigger grid for coping</ul> 
-<ul>use filters for edge dectecion, high contrast, and other thinks that may be usefull for paiting</ul>
-<ul>Complitly web-based and ipad compatible with ofline mode</ul></li>
+<li><ol>load a file from the web</ol>
+<ol>draw a secure grid over the image and calculate the bigger grid for coping</ol> 
+<ol>use filters for edge dectecion, high contrast, and other thinks that may be usefull for paiting</ol>
+<ol>Complitly web-based and ipad compatible with ofline mode</ol></li>
--- script.js
+++ script.js
@@ -1,13 +1,51 @@
+var img = new Image();
+
 function loadImage() {	
-	var url = $("#URL").val();
-	alert(url);
-	var img = new Image();
+	var url = document.getElementById("URL").value;
 	img.src = url;
+	dImage();
 }
 
-$(document).ready(function() {
-	var ctx = document.getElementById('canvas').getContext('2d');
+function loadGrid() {	
+	var size = document.getElementById("size").value;
+	colm = can_img.width / size;
+	rows = can_img.height / size;
+	dGrid(colm, rows);
+}
+
+var cont = document.getElementById('canvas');
+var can_img = document.getElementById('can_img');
+var can_grid = document.getElementById('can_grid');
+var ctx_img = can_img.getContext('2d');
+var ctx_grid = can_grid.getContext('2d');
+
+function dImage() {
 	img.onload = function() {
-		ctx.drawImage(img, 0, 0);
+		can_img.width = img.width;
+		can_img.height = img.height;
+		can_grid.width = img.width;
+		can_grid.height = img.height;
+		ctx_img.drawImage(img, 0, 0);
 	}
-});
+}
+
+function dGrid(colm, rows) {
+	ctx_grid.beginPath();
+
+	for (var x = 0; x <= can_img.width; x += colm) {
+		ctx_grid.moveTo(0.5 + x, 0);
+		ctx_grid.lineTo(0.5 + x, can_img.width);
+ 	}
+
+	for (var y = 0; y <= can_img.height; y += rows) {
+   	ctx_grid.moveTo(0, 0.5 + y);
+		ctx_grid.lineTo(can_img.height, 0.5 + y);
+	}
+
+	ctx_grid.strokeStyle = "black";
+	ctx_grid.stroke();
+}
+
+
+document.getElementById("bttn_LI").addEventListener("click", function(){loadImage()});
+document.getElementById("bttn_DG").addEventListener("click", function(){loadGrid()});
