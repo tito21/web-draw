2dcda8567626ec08c10f3e4ceef8af42d3040fcf
alberto <alberto.di.b.o@gmail.com>
2013-09-23 05:53:52 -0300

    added 2 script files

---
+++ lib.js
@@ -0,0 +1,42 @@
+// Grid Object
+
+function Grid(colm, rows, size) {
+	this.colm = colm;
+	this.rows = rows;
+	this.size = size;
+
+}
+
+Grid.prototype.dispGrid = function() { // Grid method
+	ctx.beginPath();
+
+	console.log(this.colm, this.rows);
+
+	for (var x = 0; x <= can.width; x += this.colm) {
+
+		ctx.moveTo(0.5 + x, 0.5);
+		ctx.lineTo(0.5 + x, can.height);
+	}
+
+	for (var y = 0; y <= can.height; y += this.rows) {
+		ctx.moveTo(0.5, 0.5 + y);
+		ctx.lineTo(can.width, 0.5 + y);
+	}
+
+	ctx.strokeStyle = "black";
+	ctx.stroke();
+};
+
+// Image Object
+
+function Image_Can (url) {
+	this.image = new Image();
+	this.url = url;
+	this.image.src = url;
+	this.height = this.image.height;
+	this.width = this.image.width;
+}
+
+Image_Can.prototype.dispImage = function(ctx) {
+	ctx.drawImage(this.image, 0, 0, can.width, can.height);
+};
